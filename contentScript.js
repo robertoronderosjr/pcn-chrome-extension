1f0b1cb2873b209de508e23ab35958879923b4a9
Roberto Ronderos <robertoronderosjr@gmail.com>
2020-11-14 13:21:58 -0500

    fifa 21

--- contentScript.js
+++ contentScript.js
@@ -1,52 +1,46 @@
-function cleanUsernames(usernames) {
-    var firstUsername = usernames[0]
-    var i;
-    for (i = 1; i < usernames.length; i++) {
-        if (usernames[i] === firstUsername) {
-            break;
-        }
-    }
-    var cleanUsernames = usernames.slice(0, i);
-    return cleanUsernames;
-}
+var playerIds = {}
 
-function auto_fill_stats(stats, usernames, titles) {
+function auto_fill_stats(stats) {
     stats.forEach(stat => {
-        var indexGt = usernames.indexOf(stat["Player ID"]);
+        var current_gt = stat["Player ID"];
+        var gtId = playerIds[current_gt];
         for (let [key, value] of Object.entries(stat)) {
             // iterate over each object in the file and input stats
             if (key === "Player ID") {
                 continue;
             }
-            var indexStat = titles.indexOf(key);
-            var id = 'form_personAndStatisticsList_' + indexGt + '__cumulativeStatistics_'  + (indexStat - 3) +  '__statTypeValueString'
+            var statName = key.replace(/\s+/g, '').toLowerCase();
+            var id = 'sp_players[4855][' + gtId + '][' + statName + ']';
             console.log(id);
-            $("#" + id).val(value);
+            $("input[name='" + id + "']").val(value)
           }
+//        var indexGt = usernames.indexOf(stat["Player ID"]);
+//        for (let [key, value] of Object.entries(stat)) {
+//            // iterate over each object in the file and input stats
+//            if (key === "Player ID") {
+//                continue;
+//            }
+//            var indexStat = titles.indexOf(key);
+//            var id = 'form_personAndStatisticsList_' + indexGt + '__cumulativeStatistics_'  + (indexStat - 3) +  '__statTypeValueString'
+//            console.log(id);
+//            $("#" + id).val(value);
+//          }
     });
 }
 
 $(document).ready(function () {
-    var titles = [];
-    var usernames = []
-
-    $("table.width-100 thead.thead-dark-gray tr th").each(function(){
-        titles.push($(this).text());
-    });
-
-    var elms = $.map($("th[data-label='Name']"), function (el, i) {
-        var text = el.innerText;
-        var splitText = text.split(" (")
-        var username = splitText[0]
-        usernames.push(username);
-    });
-    usernames = cleanUsernames(usernames);
-    console.log(usernames);
-
     const url = chrome.runtime.getURL('team_stats.json');
-
+    $(".sp-data-table.sp-performance-table tr td:nth-child(3)").each(function(i){
+        var text = $(this).text().trim();
+        if (text !== "Total") {
+            gt = text
+            id = $(this).parent().attr("data-player");
+            console.log("gt:"+gt+" id:"+id);
+            playerIds[gt] = id;
+        }
+    });
     fetch(url)
         .then((response) => response.json()) //assuming file contains json
-        .then((json) => auto_fill_stats(json, usernames, titles));
+        .then((json) => auto_fill_stats(json));
 
 });
